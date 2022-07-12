0a20953df329a72d2ba8d582540fcacefb361a20
Capone.s <S.Capone@lutech.it>
2022-07-12 17:47:40 +0200

    Initial commit

---
+++ src/main/groovy/universita/esami/EsamiApplication.groovy
@@ -0,0 +1,13 @@
+package universita.esami
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EsamiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(EsamiApplication, args)
+	}
+
+}
---
+++ src/test/groovy/universita/esami/EsamiApplicationTests.groovy
@@ -0,0 +1,13 @@
+package universita.esami
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EsamiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
