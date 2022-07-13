311fc4e46a3c6234cf4f524d10d269ef3184b47d
Capone.s <S.Capone@lutech.it>
2022-07-13 17:29:26 +0200

    @mod_1 - ultima modifica

--- src/main/groovy/universita/esami/domain/Libretto.groovy
+++ src/main/groovy/universita/esami/domain/Libretto.groovy
@@ -1,14 +1,6 @@
 package universita.esami.domain
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.JoinColumn
-import javax.persistence.ManyToOne
-import javax.persistence.OneToOne
-import javax.persistence.Table
+import javax.persistence.*
 
 @Entity
 @Table(name = "libretto")
