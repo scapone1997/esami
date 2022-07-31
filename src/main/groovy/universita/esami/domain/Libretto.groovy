a92070f9a9ad9d370c9f2dbf64a06b9005b6ecec
Capone.s <S.Capone@lutech.it>
2022-07-31 20:05:06 +0200

    @mod_3

--- src/main/groovy/universita/esami/domain/Libretto.groovy
+++ src/main/groovy/universita/esami/domain/Libretto.groovy
@@ -9,12 +9,17 @@ class Libretto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Integer id
+
     @Column(name="edizione_corso")
     Integer edizioneCorso
+
     Integer corso
+
     @Column(name="data_appello")
     Date data
+
     String nome
+
     String voto
 
     @ManyToOne
--- src/main/groovy/universita/esami/repository/LibrettoRepository.groovy
+++ src/main/groovy/universita/esami/repository/LibrettoRepository.groovy
@@ -1,9 +1,19 @@
 package universita.esami.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 import universita.esami.domain.Libretto
+import universita.esami.domain.Studente
 
 @Repository
 interface LibrettoRepository extends JpaRepository<Libretto, Integer>{
+
+    @Query(value = "SELECT * FROM libretto WHERE studente =:matricola", nativeQuery = true)
+    List<Libretto> findByStudente(@Param("matricola") Integer matricola)
+
+    Optional<Libretto> findByNomeAndStudente(String nome, Studente studente)
+
+    void deleteByNomeAndStudente(String nome, Studente studente)
 }
