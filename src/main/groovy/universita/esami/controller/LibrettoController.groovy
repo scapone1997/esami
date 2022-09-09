74f9db7c46d0d772228ae40724c13916bd64d6fe
Capone.s <S.Capone@lutech.it>
2022-09-09 23:53:00 +0200

    fix

--- src/main/groovy/universita/esami/controller/LibrettoController.groovy
+++ src/main/groovy/universita/esami/controller/LibrettoController.groovy
@@ -22,20 +22,20 @@ class LibrettoController {
     @Autowired
     LibrettoService librettoService
 
-    @GetMapping("/libretto/{matricola}")
+    @GetMapping("/elenco-esami/{matricola}")
     ResponseEntity<List<Libretto>> libretto(@PathVariable Integer matricola){
         List<Libretto> list = librettoService.getLibretto(matricola)
         return ResponseEntity.ok().body(list)
     }
 
     @PutMapping("/libretto")
-    ResponseEntity<String> libretto(@RequestBody Libretto libretto){
+    ResponseEntity<String> aggiornaLibretto(@RequestBody Libretto libretto){
         librettoService.aggiorna(libretto)
         return ResponseEntity.ok().body("ok")
     }
 
     @PostMapping("/carica-libretto")
-    ResponseEntity<String> inizializza(@RequestBody NuovoStudente iscritto){
+    ResponseEntity<String> inizializzaLibretto(@RequestBody NuovoStudente iscritto){
         librettoService.inizializza(iscritto)
         return ResponseEntity.ok().body("ok")
     }
