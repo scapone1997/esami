dfc70ec7b5835c500e3ee6a8fcfcd0e861b3c44c
scapone1997 <79840257+scapone1997@users.noreply.github.com>
2022-09-09 15:39:01 +0200

    Update Libretto.groovy

--- src/main/groovy/universita/esami/domain/Libretto.groovy
+++ src/main/groovy/universita/esami/domain/Libretto.groovy
@@ -9,19 +9,13 @@ class Libretto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     Integer id
-
     @Column(name="edizione_corso")
     Integer edizioneCorso
-
     Integer corso
-
     @Column(name="data_appello")
     Date data
-
     String nome
-
     String voto
-
     @ManyToOne
     @JoinColumn(name = "studente")
     Studente studente
